9dcb5dcda27a5bb71b366c03663592d379630c03
Justin <101229476+QuadNard@users.noreply.github.com>
2022-06-22 18:05:05 +0000

    updated folders

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -4,7 +4,13 @@ import { getFirestore } from "./server/@firebase/app";
 
 
 const firebaseConfig = {
-
+  apiKey: "AIzaSyDpjRlN8mt1BMibz_Fot7fT6p2WT1-pqxw",
+  authDomain: "dev-in-space.firebaseapp.com",
+  projectId: "dev-in-space",
+  storageBucket: "dev-in-space.appspot.com",
+  messagingSenderId: "597160643163",
+  appId: "1:597160643163:web:3444ec3ca3f667e9248d7e",
+  measurementId: "G-71JPMPM54W"
 };
 
 
